ef472825b8eb8538d6990cc6dfde0f7e5c8720e0
Sushil <sushilkarki352@gmail.com>
2025-04-20 20:46:43 -0500

    update command

--- codepipeline.cmd
+++ codepipeline.cmd
@@ -3,7 +3,7 @@ aws cloudformation deploy \
   --stack-name ecommerce-pipeline-stack \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    ConnectionArn="arn:aws:codeconnections:us-east-1:340233612461:connection/d04bd876-9127-419e-bfa5-d9112b459d26" \
+    ConnectionArn="your arn" \
     FullRepositoryId="Sushilkarki77/cs516-final-backend" \
     GitHubLocation="https://github.com/Sushilkarki77/cs516-final-backend" 
 
